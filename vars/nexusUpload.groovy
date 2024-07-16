e9e13617ccf8facc0445df9f9654e7cfba319ca2
Sandeep-HN <144303061+Sandeep-HN@users.noreply.github.com>
2024-07-16 09:14:32 +0530

    Update nexusUpload.groovy

--- vars/nexusUpload.groovy
+++ vars/nexusUpload.groovy
@@ -1,9 +1,11 @@
 def call(){
           def pom = readMavenPom file: 'pom.xml'
           def version = pom.version
-         def repoName = artifactId
+          def artifactId = pom.artifactId
+          def groupId = pom.groupId
+          def repoName = artifactId
           if(version.endsWith("SNAPSHOT")){
                     repoName = "${repoName}-snapshot"
                 }
-  nexusArtifactUploader artifacts: [[artifactId: 'ai-leads', classifier: '', file: 'target/ai-leads.war', type: 'war']], credentialsId: 'nexus', groupId: 'in.javahome' , nexusUrl: '23.20.166.185:8081', nexusVersion: 'nexus3', protocol: 'http', repository: "${repoName}", version: version      
+  nexusArtifactUploader artifacts: [[artifactId: artifactId , classifier: '', file: 'target/${artifactId}.war', type: 'war']], credentialsId: 'nexus', groupId: groupId , nexusUrl: '23.20.166.185:8081', nexusVersion: 'nexus3', protocol: 'http', repository: "${repoName}", version: version      
 }
